5f1f1ac5b2b284e6799eea239e1b0e83a17b6b1a
Agustín Marquardt <98858652+marcui13@users.noreply.github.com>
2025-06-16 01:10:02 -0300

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  openaiApiKey: 'SHqkTcumOrOdCmzVKJ0bz0VRfj8QUJRhfGDDDw0GAlE5QCpbn5HDY_zrma7PgxBmC8He2jArIyT3BlbkFJKA1wEVwRzyQsV9SRe24blaHD1xxBCo2eHCQHyaua5PtLCMFZcMmkauRIefUdNvRsjz66'
-};
